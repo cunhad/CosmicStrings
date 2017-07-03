93504306cf3fad6c3583fd3671ce2a4b9814238f
EloiseChakour <eloise.chakour@gmail.com>
2017-07-03 18:00:59 -0400

    Add files via upload

---
+++ Code/Normalization2D.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  3 17:51:25 2017
+
+@author: eloisechakour
+"""
+
+import normalization_array as na
+import numpy as np
+import math
+
+
